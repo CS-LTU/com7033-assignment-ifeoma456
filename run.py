93dfb81cd9a751151039d8fd01faf7ca7f5b87dd
ifeoma456 <2412136@leedstrinity.ac.uk>
2025-11-04 11:24:54 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import flask
+
+app = Flask(__name__)
+
+
+    
+
+
+
+
+@app.route('/')
+def home():
+    return '''
+        <h1> Welcome to Flask in jupyter! </h1>
+        <p> This is a Simple Webpage Served using Flask inside Jupyter.</p>
+        <a href=" /about">Go to About Page</a>
+        '''
+@app.route('/about')
+def about():
+    return'''
+        <h1>About This Web App</h1>
+        <p>This Page was generated using Flask with aJupyter notebook. It's a simple example to show Flask routing.</p>
+        <a href="/">Go back Home</a>
+        '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
